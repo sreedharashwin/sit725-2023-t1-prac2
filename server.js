6fe4356a0a8dc0aa2cdbf8f149d358691ef6f361
sreedharashwin <s223955271@deakin.edu.au>
2023-11-21 17:24:34 +1100

    Adding Post api for showing product

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
 let port = process.env.port || 3000 ;
-
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/'));
+
 
 
 app.get('/',(req,res)=>{
@@ -10,7 +11,7 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(port, ()=>{
-    console.log('server started - 2');
+    console.log('server started');
 });
 
 app.get('/addTwoNumbers',(req,res)=>{
@@ -20,4 +21,12 @@ app.get('/addTwoNumbers',(req,res)=>{
     let obj = {statusCode:200, message:'success', data:sum}
     console.log("Number1="+num1 + " and "+ "Number2="+num2);
     res.json(obj);
+});
+
+app.post('/multiplyByPost',(req,res)=>{
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+    let product = num1 * num2 ;
+    let responseOBJ = {statusCode:200, message:'success. This was from a POST call', data:product}
+    res.json(responseOBJ);
 });
